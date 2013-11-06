66f069dd6d903eb71485c0ce0d63e0b7ad5e744d
iResNovae <iresnovaeserg@agmail.com>
2013-11-06 18:33:13 +0300

    admin_controller

--- app/controllers/admin_sing_up_controller.rb
+++ app/controllers/admin_sing_up_controller.rb
@@ -1,8 +1,12 @@
 class AdminSingUpController < ApplicationController
   def admin
-#	admin = Admin.find_by_email(params[:session][:email].downcase)
-#   if admin && admin.authenticate(params[:session][:password])
-#    render 'static_pages/home'
-#  end
+  @admin = Admin.new
+	 admin_log = Admin.find_by_email(@admin[:email].downcase)
+   if admin_log != nil && admin_log.authenticate(@admin[:password])
+    render 'static_pages/home'
+    
+    else
+    render 'admin_sing_up/admin'
+  end
 end
 end
--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: admins
+#
+#  id              :integer          not null, primary key
+#  email           :string(255)
+#  password        :string(255)
+#  password_digest :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  remember_token  :string(255)
+#
+
 require 'spec_helper'
 
 describe Admin do
