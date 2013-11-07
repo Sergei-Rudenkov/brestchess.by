44071c4fb92c075093ce584dfac1190056e302eb
iResNovae <iresnovaeserg@agmail.com>
2013-11-07 13:56:18 +0300

    session_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,15 @@
-Katashuk::Application.routes.draw do
-  get "admin_sing_up/admin"
-
+Katashuk::Application.routes.draw do  
+resources :sessions, only: [:new, :create, :destroy]
+  #root to: 'static_pages#home'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   get "static_pages/home"
-
   get "static_pages/contacts"
-
   get "static_pages/news"
-
   get "static_pages/tournaments"
-  
   get "static_pages/soon"
-  
   get "static_pages/trips"
-    
   get "static_pages/lit"
-      
   get "static_pages/couchroom"
 
 
